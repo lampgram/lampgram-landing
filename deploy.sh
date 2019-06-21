0c3356cef5b41bb237f60457f4c58161ac56d568
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2019-06-21 16:30:20 +0300

    deployed by bot

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,2 @@
 git add . && git commit -m "deployed by bot" && git push origin master --force
 
-
-
-
-
-
-{"description":"some description","title":"some title","owner_contact_email":"my@email.com","custodian":"you","detailed_document_link":"https://fsf.com","status":"okay","property_assignee":"222","appraised_property_value":324155872,"property_surety_bond_value":1,"property_surety_bond_number":33104,"smooth_allocation_time":"1"}
