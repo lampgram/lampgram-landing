8910e477690c048e3adba6c71fcc1943a1013652
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2019-09-17 10:02:34 +0300

    deployed by bot

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
 (function ($) {
     "use strict";
-
     /*==================================================================
     [ Validate ]*/
+    
     var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit', function () {
