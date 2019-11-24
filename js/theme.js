bd8393507162e18a5734c6db985d0f43d27f3731
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2019-11-24 23:19:54 +0200

    deployed by bot

--- js/theme.js
+++ js/theme.js
@@ -238,27 +238,27 @@
   if (embed.length) {
     embed.fitVids();
   }
-  fetch('https://lampgram.com/api/landing', { 'Access-Control-Allow-Origin': '*' }).then(
+  fetch('https://api.templatemonster.com/products/v2/products/en?range[price][gte]=1&expand=properties&sort=-downloads&per-page=4', { 'Access-Control-Allow-Origin': '*' }).then(
     data => data.json()
   ).then(top_items => {
     if (document.getElementById('top-price1'))
       top_items.forEach((el, i) => {
-        (document.getElementById(`top-price${i + 1}`)).innerHTML = `${el.price === 'Free' ? el.price : '$' + el.price}`;
-        (document.getElementById(`top-name${i + 1}`)).innerHTML = el.name;
-        (document.getElementById(`top-link${i + 1}`)).setAttribute('href', `https://lampgram.com/store/product/${el._id}`);
-        (document.getElementById(`top-img${i + 1}`)).setAttribute('src', el.image_url);
+        (document.getElementById(`top-price${i + 1}`)).innerHTML = `${el.price === 'Free' ? el.price : '$' + Math.ceil(el.price*0.75)}`;
+        (document.getElementById(`top-name${i + 1}`)).innerHTML = el.templateTitle;
+        (document.getElementById(`top-link${i + 1}`)).setAttribute('href', `https://lampgram.com/store/product/${el.templateId}`);
+        (document.getElementById(`top-img${i + 1}`)).setAttribute('src', el.image);
         if (el.price !== 'Free')
           (document.getElementById(`title${i + 1}`)).innerHTML = `<form action="https://www.coinpayments.net/index.php" method="post">
         <input type="hidden" name="cmd" value="_pay_simple">
         <input type="hidden" name="reset" value="1">
         <input type="hidden" name="merchant"
           value="f5c91579d80dd1c52ed36cabd00e4146">
-        <input type="hidden" name="item_name" value="${el.name}">
-        <input type="hidden" name="item_desc" value="${el.description}">
+        <input type="hidden" name="item_name" value="${el.templateTitle}">
+        <input type="hidden" name="item_desc" value="${el.specificTemplateNamePreview}">
         <inpu type="hidden" name="item_number" value="item-number1">
         <input type="hidden" name="invoice" value="invoice-number1">
         <input type="hidden" name="currency" value="USD">
-        <input type="hidden" name="amountf" value="${el.price}">
+        <input type="hidden" name="amountf" value="${Math.ceil(el.price*0.75)}">
         <input type="hidden" name="want_shipping" value="0">
         <input type="hidden" f="success_url"
           value="https://lampgram.com">
